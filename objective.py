d3666c462a17ce80a33680db4fbad28f19b9e7d6
GpAstA <yuu3442@gmail.com>
2024-07-24 20:49:11 +0900

    testで評価

--- objective.py
+++ objective.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 from model.LLGMN import LLGMN
 from model.elasticnet_reducer import ElasticNetDimensionReducer
 
-def objective(trial, train_dataloader, in_features, n_class, n_component, n_epoch):
+def objective(trial, train_dataloader, test_dataloader, in_features, n_class, n_component, n_epoch):
     # ハイパーパラメータのサンプリング
     alpha = trial.suggest_float('alpha', 0.0, 1.0)
     l1_ratio = trial.suggest_float('l1_ratio', 0.0, 1.0)
@@ -26,8 +26,6 @@ def objective(trial, train_dataloader, in_features, n_class, n_component, n_epoc
         model.train()
         reducer.train()
         total_loss = 0
-        correct = 0
-        total = 0
 
         for inputs, targets in train_dataloader:
             inputs, targets = inputs.to(device), targets.to(device)
@@ -51,23 +49,47 @@ def objective(trial, train_dataloader, in_features, n_class, n_component, n_epoc
 
             total_loss += loss.item()
 
+            # # 精度の計算
+            # _, predicted = torch.max(outputs.data, 1)
+            # total += targets.size(0)
+            # correct += (predicted == targets).sum().item()
+
+        avg_loss = total_loss / len(train_dataloader)
+        # accuracy = 100 * correct / total
+        # print(f'Epoch {epoch+1}/{n_epoch}, Loss: {avg_loss}, Accuracy: {accuracy}%')
+
+        # # ベスト精度の更新
+        # if accuracy > best_accuracy:
+        #     best_accuracy = accuracy
+    
+    # テストループ
+    model.eval()
+    reducer.eval()
+    total_test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, targets in test_dataloader:
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            # フォワードパス
+            reduced_inputs = reducer(inputs)
+            outputs = model(reduced_inputs)
+
+            # 損失の計算
+            loss = criterion(outputs, targets)
+            total_test_loss += loss.item()
+
             # 精度の計算
             _, predicted = torch.max(outputs.data, 1)
             total += targets.size(0)
             correct += (predicted == targets).sum().item()
 
-        avg_loss = total_loss / len(train_dataloader)
-        accuracy = 100 * correct / total
-        # print(f'Epoch {epoch+1}/{n_epoch}, Loss: {avg_loss}, Accuracy: {accuracy}%')
+    avg_test_loss = total_test_loss / len(test_dataloader)
+    accuracy = 100 * correct / total
+    # print(f'Test Loss: {avg_test_loss}, Accuracy: {accuracy}%')
 
-        # ベスト精度の更新
-        if accuracy > best_accuracy:
-            best_accuracy = accuracy
-
-        # NaNチェック
-        if torch.isnan(loss) or torch.isinf(loss):
-            return float('inf')
 
     # 結果として損失を返すが、精度も記録する
-    trial.set_user_attr('accuracy', best_accuracy)
-    return avg_loss
+    trial.set_user_attr('accuracy', accuracy)
+    return avg_test_loss
